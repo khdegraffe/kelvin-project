d253424056b5d4692cbbf2ac78b8b71e734d701b
khdegraffe <khdegraffe@gmail.com>
2024-10-24 20:49:25 -0400

    Create main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 //PROJECT: KELVIN WEATHER
 
-//Today's forecast, described by a variable named kelvin set to the forecasted tempature in kelvin
+//Today's forecast, described by a variable named kelvin set to the forecasted temperature in kelvin
 const kelvin = 0
 
-//Today's forecast in Celsisu, which is equal to the kelvin temperature minus 273
+//Today's forecast in Celsius, which is equal to the kelvin temperature minus 273
 const celsius = (kelvin - 273);
 console.log(celsius);
 //Today's forecast in Fahrenheit, which was calculated by multiplying the celsius temperature by 9/5 and then adding 32
 let fahrenheit = (celsius * (9/5) + 32);
 
-//The .floor() method used to round down the Fahrenheit temp, since it's often a decimal number when converted from Celsius. To assign this I delcared the farhenheit variable as 'let' which I'm not sure was correct.
-fahrenheit = Math.floor(fahrenheit);
-
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+//The .floor() method used to round down the Fahrenheit temp, since it's often a decimal number when converted from Celsius.
 
 // 0 Kelvin in Fahrenheit is -460 degrees. 
 
